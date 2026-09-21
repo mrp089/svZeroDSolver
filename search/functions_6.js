736a79beab3e8b3a0d5f313c4b7642f4126ed120
mrp089 <mrp089@users.noreply.github.com>
2026-09-21 22:20:31 +0000

    deploy: 8a0c68e6c8207186848d73886bce4d9075fa5655

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['inputparameter_0',['InputParameter',['../struct_input_parameter.html#a8192e830399ab16439dd2e4c7e4a5d3d',1,'InputParameter']]],
-  ['integrator_1',['Integrator',['../class_integrator.html#ad6e4f6a6409bec36c05ddc6a3fead000',1,'Integrator::Integrator(Model *model, double time_step_size, double rho, double atol, int max_iter)'],['../class_integrator.html#a91fdfba81971dfa13ba22521d11b7c46',1,'Integrator::Integrator()']]]
+  ['halfcosineactivation_0',['HalfCosineActivation',['../class_half_cosine_activation.html#a0b1afb42cd592e1ad6dbc884e505c398',1,'HalfCosineActivation']]],
+  ['has_5fblock_1',['has_block',['../class_model.html#abc36e7c0be46ee4c31f954e9312251d3',1,'Model']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,14 +1,14 @@
 var indexSectionsWithContent =
 {
   0: "12_abcdefghijklmnopqrstuvwyz~",
-  1: "abcdfhijlmnoprstvw",
+  1: "abcdefhijlmnoprstvw",
   2: "abcdfijlmnoprsvw",
-  3: "abcfghijlmnoprstuvwz~",
+  3: "abcdfghijlmnoprstuvwz~",
   4: "abcdefgijmnoprstuvy",
   5: "b",
   6: "bepv",
   7: "ceiklrtv",
-  8: "_d",
+  8: "_de",
   9: "12abcdefghijlmnopqrstuvw"
 };
 
